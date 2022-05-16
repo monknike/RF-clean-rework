bb27e9b43c6693157fff96b2fda4bc876e2f40ef
monknike <mayanknaik2000@gmail.com>
2022-05-17 00:30:57 +0100

    Update Tackle.cs

--- RDVFSharp/Commands/Ingame/Tackle.cs
+++ RDVFSharp/Commands/Ingame/Tackle.cs
@@ -10,26 +10,34 @@ namespace RDVFSharp.Commands
     {
         public override void ExecuteCommand(string character, IEnumerable<string> args, string channel)
         {
+
+            var tackleok = 0;
+
+            
             var attacker = Plugin.CurrentBattlefield.GetActor();
             var target = Plugin.CurrentBattlefield.GetTarget();
-            var others = Plugin.CurrentBattlefield.Fighters.Where(x => x.Name != attacker.Name).OrderBy(x => new Random().Next()).ToList();
-            var otherothers = Plugin.CurrentBattlefield.Fighters.Where(x => x.Name != target.Name).OrderBy(x => new Random().Next()).ToList();
+            var others = Plugin.CurrentBattlefield.Fighters.Where(x => x.Name != attacker.Name);
+            var otherothers = Plugin.CurrentBattlefield.Fighters.Where(x => x.Name != target.Name);
 
-
-            foreach (var otherother in otherothers)
-
-            {
-                foreach (var other in others)
-                {
+            foreach (var other in others)
+            { 
+                foreach (var otherother in otherothers)
+                { 
                     if ((!otherother.IsGrappling(target) && !target.IsGrappling(otherother) && !attacker.IsGrappling(other) && !other.IsGrappling(attacker) && !target.IsGrappling(attacker) && !attacker.IsGrappling(target) && (attacker.IsGrabbable != target.IsGrabbable) || (attacker.IsGrabbable == 0)))
                     {
-                        base.ExecuteCommand(character, args, channel);
+                        tackleok += 1;
                     }
                     else
                     {
-                        Plugin.FChatClient.SendMessageInChannel("You can't use Tackle when you already are in grappling range.", Plugin.Channel);
+                        Plugin.FChatClient.SendMessageInChannel("You can't use Tackle when you already are in grappling range, or on someone that's being grappled by/grappling someone else.", Plugin.Channel);
                     }
                 }
+            }
+
+            if (tackleok == 1)
+            {
+                base.ExecuteCommand(character, args, channel);
+                tackleok = 0;
             }
         }
     }
